ade85a0b4cdbda0ddce434032770063dc1b0120c
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-09-29 20:14:53 +0000

    chore: remove unnecessary comments from userprofile/views.py

--- userprofile/views.py
+++ userprofile/views.py
@@ -101,19 +101,17 @@ def edit_review(request, pk):
         form = ReviewForm(request.POST, instance=review)
         if form.is_valid():
             review.save()
-        return redirect("review_dashboard")  # Redirects User to dashboard
-        # with all their reviews
+        return redirect("review_dashboard")
     else:
         if request.user == review.poster:
             form = ReviewForm(instance=review)
         else:
-            return redirect("review_dashboard")  # Redirects User to dashboard
+            return redirect("review_dashboard")
     return render(
         request,
         "userprofile/edit_review.html",
         {"form": form, "review": review}
     )
-
 
 # Delete a review
 @login_required
@@ -121,4 +119,3 @@ def delete_review(request, pk):
     review = Review.objects.get(pk=pk)
     review.delete()
     return redirect("review_dashboard")  # Redirects User to dashboard
-    # with all their reviews
